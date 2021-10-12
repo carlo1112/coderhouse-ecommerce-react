4d3fdb7c3233ccd2801ff4a2b7ffd2276bd79ef0
Carlos Cattaneo <carlo1112@gmail.com>
2021-10-12 19:17:22 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+//Variable React en scope
+//nodemodules -> carpeta react -> react.js -> traer variable React
 import React from "react"
+
+//Variable React en scope
+//nodemodules -> carpeta react-dom -> index.js -> traer ReactDOM
 import ReactDOM from "react-dom"
+
+//Tiene que tener un elemento 
 const Elemento = () => <p>Hola React</p>
+
+//Renderizado del elemento. (primer parametro: Elemento, segundo parametro: lugar donde se va a mostrar)
 ReactDOM.render(<Elemento />, document.getElementById("root"))
